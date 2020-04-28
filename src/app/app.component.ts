12183c8299f61a65900c00710b66ca5a0ca0d0e8
Moritz Munte <geschan@users.noreply.github.com>
2020-04-28 12:57:39 +0200

    Add cordova with auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+// Import Auth0Cordova
+import Auth0Cordova from '@auth0/cordova';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +25,11 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      // Redirect back to app after authenticating
+      (window as any).handleOpenURL = (url: string) => {
+        Auth0Cordova.onRedirectUri(url);
+      }
     });
   }
 }
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+
+console.log('[CUSTOM WEBPACK LOADED]');
+
+module.exports = {
+  node: {
+    crypto: true
+  }
+};
